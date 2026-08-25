201f9677693bb69e360f7cb100d112551bc59fda
Yuvraj Rode <rodeyuvraj2@gmail.com>
2026-08-25 20:36:25 +0530

    Time: 2 ms (98.37%), Space: 52.6 MB (46.21%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -5,10 +5,12 @@ class Solution {
         for(int i=0; i< nums.length; i++) {
             if (nums[i] == 1) {
                 count += 1;
-                max_count = Math.max(max_count, count);
             }
             else{
                 count = 0;
+            }
+            if (count > max_count) {
+                max_count = count;
             }
         }
         return max_count;
