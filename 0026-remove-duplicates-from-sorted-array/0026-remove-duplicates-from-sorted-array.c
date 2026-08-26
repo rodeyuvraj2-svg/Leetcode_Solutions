94322d1856feae714cddbf24c76559dfb9f19271
Yuvraj Rode <rodeyuvraj2@gmail.com>
2026-08-26 15:23:21 +0530

    Time: 0 ms (100%), Space: 13 MB (6.68%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -1,10 +1,6 @@
 int removeDuplicates(int* nums, int numsSize) {
     int left = 0, right = 1;
 
-    if (numsSize == 0) {
-        return 0;
-    }
-
     while (right < numsSize) {
         if (nums[left] == nums[right]) {
             right ++;
